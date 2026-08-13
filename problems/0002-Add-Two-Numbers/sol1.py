09d6016788d9bca9c8faa161c4da11c20a8beda9
ishwari2609 <ishwaribole09@gmail.com>
2026-08-13 16:26:44 +0530

    Add sol1: 2. Add Two Numbers (Python)

---
+++ problems/0002-Add-Two-Numbers/sol1.py
@@ -0,0 +1,23 @@
+# ==========================================================
+# 2. Add Two Numbers
+# Difficulty : Medium
+# Language   : Python
+# Solution   : #1
+# Runtime    : 0 ms (Beats 100%)
+# Memory     : 19.4 MB (Beats 41%)
+# Link       : https://leetcode.com/problems/add-two-numbers/
+# ==========================================================
+
+        while l1 or l2 or carry:
+
+        current = dummy
+        carry = 0
+        dummy = ListNode(0) 
+    :   
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Option[ListNode]
+class Solution:
+#         self.next = next
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+# class ListNode:
+# Definition for singly-linked list.
